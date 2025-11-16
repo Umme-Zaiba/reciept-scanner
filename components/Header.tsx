42aab6e672a1f373511268465d5dbe75ff600601
Umme Zaiba <ummezaiba24j@gmail.com>
2025-11-16 14:40:53 +0530

    Update Home component greeting and enhance Header with authentication buttons; update package dependencies

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Shield } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
-  return <div>Header</div>;
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
+
+  return (
+    <div
+      className={`p-4 flex justify-between items-center ${isHomePage ? "bg-blue-50" : "bg-white border-b border-blue-50"}`}
+    >
+      <Link href="/" className="flex items-center gap-2">
+        <Shield className="w-8 h-8" />
+        <h1 className="text-xl font-semibold">Expensio</h1>
+      </Link>
+
+      <div className="flex space-x-4 items-center">
+        <SignedIn>
+          <Link href="/reciepts">
+            <Button variant="outline">My reciepts</Button>
+          </Link>
+
+          <Link href="/manage-plan">
+            <Button>Manage plan</Button>
+          </Link>
+
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Login</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </div>
+  );
 }
